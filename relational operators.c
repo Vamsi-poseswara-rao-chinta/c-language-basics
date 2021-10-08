7805e218d21a3d23d276a6c768bd6094f70e11f1
Vamsi-poseswara-rao-chinta <92150267+Vamsi-poseswara-rao-chinta@users.noreply.github.com>
2021-10-08 15:23:58 +0530

    Add files via upload

---
+++ relational operators.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a=70,b=90;
+	printf("%d\n",a<b);
+	printf("%d\n",a>b);
+	printf("%d\n",a>=b);	
+	printf("%d\n",a<=b);
+	printf("%d\n",a==b);
+	printf("%d\n",a!=b);
+	
+}
