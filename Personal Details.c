3133451dd3e59f864f4aa27e85e7997ff73d97e7
Vamsi-poseswara-rao-chinta <92150267+Vamsi-poseswara-rao-chinta@users.noreply.github.com>
2021-10-09 13:47:42 +0530

    Add files via upload

---
+++ Personal Details.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	printf("CHINTA SRINIVASA GANESH\n");
+	printf("CHINTA KOTASATYAVTI\n");
+	printf("CHINTA SATYA NAGA GOWTHAM\n");
+	printf("CHINTA VAMSI POSESWARA RAO\n");
+}
