4a745cf7c82a19f2851ba4c4c56e895e90328ceb
Vamsi-poseswara-rao-chinta <92150267+Vamsi-poseswara-rao-chinta@users.noreply.github.com>
2021-10-09 14:09:59 +0530

    Add files via upload

---
+++ increment or decrement operators.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d",&a);
+	b=++a;
+	printf("%d\n",b);
+	b=--a;
+	printf("%d\n",b);
+	b=a++;
+	printf("%d\n",b);
+	b=a--;
+	printf("%d\n",b);
+	
+	
+}
+	
+
