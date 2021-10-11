b9d33d94f57c0384f6812e5a1572dd16946f1109
Vamsi-poseswara-rao-chinta <92150267+Vamsi-poseswara-rao-chinta@users.noreply.github.com>
2021-10-11 14:26:41 +0530

    Add files via upload

---
+++ char read by else if.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+	scanf("%c",&ch);
+	if(ch=='a')
+	{
+		printf("vowels");
+	}
+	else if(ch=='e')
+	{
+		printf("vowels");
+	}
+	else if(ch=='i')
+	{
+		printf("vowels");
+	}
+	else if(ch=='o')
+	{
+		printf("vowels");
+	}
+	else if(ch=='u')
+	{
+		printf("vowels");
+	}
+	else
+	{
+		printf("consonants");
+	}
+}
